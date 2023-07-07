d7d5d618d90f2b8195be6ce4a2535a7ffcac8ce3
Njokom <knjokom@calstatela.edu>
2023-07-07 12:34:52 -0700

    refactoring -- following siraj -with minor divergences

--- compiler.py
+++ compiler.py
@@ -1,64 +1,71 @@
-import re
 import copy
+import re
 
-numbers = re.compile(r"[0-9]")
-alphabet = re.compile(r"[a-zA-Z]")
-
-def tokenizer(input):
+def tokenize(input_expression):
+    current = 0
     tokens = []
-    filtered_input = input.strip()
-    position = 0
+    alphabet_pattern = re.compile(r"[a-z]", re.I)
+    numbers_pattern = re.compile(r"[0-9]")
+    whitespace_pattern = re.compile(r"\s")
 
-    while position < len(filtered_input):
-        character = filtered_input[position]
+    while current < len(input_expression):
+        char = input_expression[current]
 
-        if character == "(":
-            tokens.append({'type': 'left_parenthesis', 'value': '('})
-            position += 1
-        elif character == ")":
-            tokens.append({'type': 'right_parenthesis', 'value': ')'})
-            position += 1
-        elif re.match(numbers, character):
-            value = ""
+        if re.match(whitespace_pattern, char):
+            current += 1
+            continue
 
-            while position < len(filtered_input) and re.match(numbers, character):
-                value += character
-                position += 1
+        if char == '(':
+            tokens.append({
+                'type': 'left_paren',
+                'value': '('
+            })
+            current += 1
+            continue
 
-                if position < len(filtered_input):
-                    character = filtered_input[position]
+        if char == ')':
+            tokens.append({
+                'type': 'right_paren',
+                'value': ')'
+            })
+            current += 1
+            continue
 
-            tokens.append({'type': 'number', 'value': value})
-        elif re.match(alphabet, character):
-            value = ""
+        if re.match(numbers_pattern, char):
+            value = ''
+            while re.match(numbers_pattern, char):
+                value += char
+                current += 1
+                char = input_expression[current]
+            tokens.append({
+                'type': 'number',
+                'value': value
+            })
+            continue
 
-            while position < len(filtered_input) and re.match(alphabet, character):
-                value += character
-                position += 1
+        if re.match(alphabet_pattern, char):
+            value = ''
+            while re.match(alphabet_pattern, char):
+                value += char
+                current += 1
+                char = input_expression[current]
+            tokens.append({
+                'type': 'name',
+                'value': value
+            })
+            continue
 
-                if position < len(filtered_input):
-                    character = filtered_input[position]
-
-            tokens.append({'type': 'name', 'value': value})
-        else:
-            position += 1
+        raise ValueError('Invalid character: ' + char)
 
     return tokens
 
 
-
-
-def parser(tokens):
+def parse(tokens):
     current = 0
 
     def walk():
         nonlocal current
-
-        if current >= len(tokens):
-            return None
-
         token = tokens[current]
-        print("Current token:", token)
 
         if token.get('type') == 'number':
             current += 1
@@ -67,36 +74,23 @@ def parser(tokens):
                 'value': token.get('value')
             }
 
-        if token.get('type') == 'left_parenthesis':
+        if token.get('type') == 'left_paren':
             current += 1
             token = tokens[current]
             node = {
                 'type': 'CallExpression',
                 'name': token.get('value'),
-                'parameters': []
+                'params': []
             }
-
             current += 1
             token = tokens[current]
-            while token.get('type') != 'right_parenthesis':
-                child_node = walk()
-                if child_node is not None:
-                    node['parameters'].append(child_node)
-                else:
-                    break  # Return if child_node is None
+            while token.get('type') != 'right_paren':
+                node['params'].append(walk())
                 token = tokens[current]
-
             current += 1
             return node
 
-        if token.get('type') == 'name':
-            current += 1
-            return {
-                'type': 'Identifier',
-                'name': token.get('value')
-            }
-
-        raise TypeError(f"Unexpected token type: {token.get('type')}")
+        raise TypeError(token.get('type'))
 
     ast = {
         'type': 'Program',
@@ -105,43 +99,42 @@ def parser(tokens):
 
     while current < len(tokens):
         ast['body'].append(walk())
-    
+
     return ast
 
 
-def traverser(ast, visitor):
-    def traverseArray(array, parent):
+def traverse(ast, node_handlers):
+    def traverse_array(array, parent):
         for child in array:
-            traverseNode(child, parent)
+            traverse_node(child, parent)
 
-    def traverseNode(node, parent):
-        method = visitor.get(node['type'])
-        if method:
-            method(node, parent)
-        elif node['type'] == 'Program':
-            traverseArray(node['body'], node)
+    def traverse_node(node, parent):
+        handler = node_handlers.get(node['type'])
+        if handler:
+            handler(node, parent)
+
+        if node['type'] == 'Program':
+            traverse_array(node['body'], node)
         elif node['type'] == 'CallExpression':
-            traverseNode(node['callee'], node)
-            traverseArray(node['arguments'], node)  # Add 'node' as the second argument
+            traverse_array(node['params'], node)
         elif node['type'] == 'NumberLiteral':
-            0
+            pass
         else:
             raise TypeError(node['type'])
 
-    traverseNode(ast, None)
+    traverse_node(ast, None)
 
 
-def transformer(ast):
-    newAst = {
+def transform(ast):
+    new_ast = {
         'type': 'Program',
         'body': []
     }
+    old_ast = ast
+    ast = copy.deepcopy(old_ast)
+    ast['_context'] = new_ast.get('body')
 
-    oldAst = ast
-    ast = copy.deepcopy(oldAst)
-    ast['_context'] = newAst.get('body')
-
-    def CallExpressionTraverse(node, parent):
+    def traverse_call_expression(node, parent):
         expression = {
             'type': 'CallExpression',
             'callee': {
@@ -150,68 +143,84 @@ def transformer(ast):
             },
             'arguments': []
         }
-        node['_context'] = parent['_context']  # Update this line
+        node['_context'] = expression['arguments']
 
-        # Store nested call expression references
         if parent['type'] != 'CallExpression':
             expression = {
                 'type': 'ExpressionStatement',
                 'expression': expression
             }
-        
-        # Store the expression in the context property
+
         parent['_context'].append(expression)
 
-
-    def NumberLiteralTraverse(node, parent):
+    def traverse_number_literal(node, parent):
         parent['_context'].append({
             'type': 'NumberLiteral',
             'value': node['value']
         })
 
-    traverser(ast, {
-        'NumberLiteral': NumberLiteralTraverse,
-        'CallExpression': CallExpressionTraverse
+    traverse(ast, {
+        'NumberLiteral': traverse_number_literal,
+        'CallExpression': traverse_call_expression
     })
 
-    return newAst
+    return new_ast
 
 
-def codeGenerator(node):
+def generate_code(node):
     if node['type'] == 'Program':
-        return '\n'.join(codeGenerator(child) for child in node['body'])
+        return '\n'.join([code for code in map(generate_code, node['body'])])
     elif node['type'] == 'Identifier':
         return node['name']
     elif node['type'] == 'NumberLiteral':
         return node['value']
     elif node['type'] == 'ExpressionStatement':
-        expression = codeGenerator(node['expression'])
-        return f"{expression};"
+        expression = generate_code(node['expression'])
+        return '%s;' % expression
     elif node['type'] == 'CallExpression':
-        callee = codeGenerator(node['callee'])
-        arguments = ', '.join(codeGenerator(arg) for arg in node['arguments'])
-        return f"{callee}({arguments})"
+        callee = generate_code(node['callee'])
+        params = ', '.join([code for code in map(generate_code, node['arguments'])])
+        return "%s(%s)" % (callee, params)
     else:
         raise TypeError(node['type'])
 
 
+def compile_program(input_expression):
+    tokens = tokenize(input_expression)
+    ast = parse(tokens)
 
+    def handle_program(node, parent):
+        # Custom handling for Program nodes
+        print("Handling Program node:", node)
 
+    def handle_call_expression(node, parent):
+        # Custom handling for CallExpression nodes
+        print("Handling CallExpression node:", node)
 
-def compiler(input):
-    tokens = tokenizer(input)
-    ast = parser(tokens)
-    newAst = transformer(ast)
-    output = codeGenerator(newAst)
+    def handle_number_literal(node, parent):
+        # Custom handling for NumberLiteral nodes
+        print("Handling NumberLiteral node:", node)
+
+    node_handlers = {
+        'Program': handle_program,
+        'CallExpression': handle_call_expression,
+        'NumberLiteral': handle_number_literal
+    }
+
+    traverse(ast, node_handlers)
+    new_ast = transform(ast)
+    output = generate_code(new_ast)
+
     return output
 
 
 def main():
-    # Test
-    input = "(add 2 (subtract 4 2))"
-    output = compiler(input)
+    input_expression = "(add 2 (subtract 4 2))"
+    output = compile_program(input_expression)
     print(output)
 
 
 if __name__ == "__main__":
     main()
+
+
